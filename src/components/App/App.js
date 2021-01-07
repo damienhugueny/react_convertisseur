209176ea4e8981f78ae33c969703f4a63b08ae61
damien <damien.hugueny@my-computing.net>
2021-01-07 15:12:28 +0100

    Une façon plus simple d'utiliser une state avec un plugin babel

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,23 +6,91 @@ import Header from '../Header/Header';
 import Currencies from '../Currencies/Currencies';
 import Amount from '../Amount/Amount';
 
-import data from '../../data/currencies';
+import currenciesList from '../../data/currencies';
 
 import './app.scss'
 
+// pour gérer un state en React (état de mon application), 2 possibilités :
+// - transformer le composant en class
+// - utiliser le hook useState
+
+// un composant React c'est une fonction... sauf si on a besoin d'utiliser un state
+
+// composant sous forme de class : render retourne le JSX
 
 // == Composant
-const App = () => {
+class App extends React.Component {
+  // constructeur : on passe props en paramètre, même si on n'en a pas
+  //constructor(props) {
+    // on appelle le constructeur de React.Component
+  //  super(props);
+    // on crée le state de l'application
+  //  this.state = {
+  //      open : false,
+  //  };
+    // sur les méthodes que j'écris, il faut que j'attache ("bind") this
+    // https://javascript.info/bind
+    // je remplace la méthode par une version enrichie avec this
+    // this.handleClick = this.handleClick.bind(this);
+  //}
 
-  return (
-    <div className="converter">
-      <Header />
-      <Currencies />
-      <Amount />
-    </div>
+  // !!! grâce au plugin babel "@babel/plugin-proposal-class-properties"
+  // je peux définir un state sans avoir besoin d'écrire un constructeur
+  state = {
+      open : false,
+  };
 
-  );
+  // !!! grâce au plugin babel "@babel/plugin-proposal-class-properties"
+  // au lieu de définir une méthode, je définis une propriété
+  handleClick = () => {
+    // j'ai accès à this sans avoir besoin de "bind"
+    // console.log("click");
+    // changer quelque chose dans le state : envoi de la nouvelle valeur avec setState
+    // on fournit un objet qui décrit les changements à appliquer au state
+    this.setState({
+      // on inverse la valeur de open avec "!"
+      //
+      open : !this.state.open,
+    });
+    // dès que le State (ou les props) change, React refait le rendu du composant
+    // rendu du composant +> rappelle la méthode render
+  };
+
+  render() {
+    // strictement équivalent à :
+    // const open = this.state.open
+    const{ open } = this.state;
+
+    // Js sait que pour que la condition totale soit vraie, les deux
+    // sous-conditions doivent être vraies. Donc si la premièrer est fausse,
+    // il n'évaluera pas la deuxième
+    // if ( condition1 && condition2) {
+    // }
+
+    // Affichage conditionnel
+    // {open &&<Currencies currencies={currenciesList}/>}
+    // => le composant Currencies est affiché seulement si open est true
+    // parenthèses si JSX est sur plusieurs lignes
+    // {open &&(
+    //   <div>
+    //     Hello
+    //   </div>
+    // )}
+
+    return (
+      <div className="converter">
+
+        <Header />
+        <button type="button" onClick={this.handleClick}>Open</button>
+        {open &&<Currencies currencies={currenciesList}/>}
+        <Amount />
+
+      </div>
+
+    );
+  }
 };
+
 
 // == Export
 export default App;
--- src/components/Currencies/Currencies.js
+++ src/components/Currencies/Currencies.js
@@ -1,18 +1,33 @@
 // == Import npm
 import React from 'react';
 
+// librairie pour valider les type de chaques props;
+import PropTypes from 'prop-types';
+
 import './currencies.scss'
 
-const Currencies = () => (
+const Currencies = ({ currencies }) => (
   <main className="currencies">
     <h2 className="currencies-title">Currencies</h2>
     <ul className="currencies-list">
-      <li className="currency">USD</li>
-      <li className="currency">CAD</li>
-      <li className="currency">JPY</li>
-      <li className="currency">AUD</li>
+
+      {currencies.map((currency) => (
+        <li key={currency.name} className="currency">{currency.name}</li>
+      ))}
+
     </ul>
   </main>
 );
+
+Currencies.propTypes = {
+  // Ok, mais on peut faire plus précis
+  // currencies: PropTypes.arrayOf(propTypes.object).isRequired,
+  currencies: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      rate: PropTypes.number.isRequired,
+    }).isRequired,
+  ).isRequired,
+};
 
 export default Currencies;
